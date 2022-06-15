932362b72e436196d0534620ca10989cdc7e3226
a.pilote <antoinepilote1@hotmail.com>
2022-06-14 20:00:34 -0400

    Cleaned up

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,16 @@
 ﻿#include <NuakeRenderer/Window.h>
-#include <NuakeRenderer/NuakeRenderer.h>
-
-// UI components
 #include <NuakeUI/NuakeUI.h>
 
 #include "MyInputManager.h"
 
-#include <stdio.h>
-
 class myDataModel
 {
 public:
-	myDataModel()
-	{
-
-	}
-
-	~myDataModel()
-	{
-
-	}
+	myDataModel() = default;
+	~myDataModel() = default;
 
 	int tabSelected = 0;
 };
-
-
-using namespace NuakeUI;
 
 class Main
 {
@@ -34,37 +19,40 @@ public:
 
 	auto LoadCanvas()
 	{
+		using namespace NuakeUI;
 		// Load HTML file.
 		auto canvas = CanvasParser::Get().Parse("../resources/UI/demo.html");
 		
 		// Creating data model and binding it to a node.
 		auto dataModel = DataModel::New("myDataModel");
 		dataModel->Bind("tab", &TabDataModel.tabSelected);
+		canvas->GetRootNode()->SetDataModel(dataModel);
 
-		canvas->SetDataModel(dataModel);
-
-		//ButtonRef btnContainer;
-		//if (canvas->FindNodeByID<Button>("btn-container", btnContainer))
-		//{
-		//	auto callback = [this](NuakeUI::Button& btn)
-		//	{
-		//		TabDataModel.tabSelected = btn.GetIndex();
-		//	};
-		//
-		//	for (uint32_t i = 0; i < btnContainer->GetChildrens().size(); i++)
-		//	{
-		//		ButtonRef button = btnContainer->GetChild<Button>(i);
-		//		button->SetClickCallback((callback));
-		//	}
-		//}
+		ButtonPtr btnContainer;
+		if (canvas->FindNodeByID<Button>("btn-container", btnContainer))
+		{
+			auto callback = [this](NuakeUI::Button& btn)
+			{
+				TabDataModel.tabSelected = btn.GetIndex();
+			};
+		
+			for (uint32_t i = 0; i < btnContainer->GetChildrens().size(); i++)
+			{
+				ButtonPtr button = btnContainer->GetChild<Button>(i);
+				button->SetClickCallback((callback));
+			}
+		}
 
 		return canvas;
 	}
 
 	Main() : TabDataModel(myDataModel())
 	{
-		auto window = NuakeRenderer::Window("NuakeUI Demo", { 1280, 720 });
-		NuakeRenderer::ApplyNuakeImGuiTheme();
+		using namespace NuakeRenderer;
+		using namespace NuakeUI;
+		
+		auto window = Window("NuakeUI Demo", { 1280, 720 });
+		ApplyNuakeImGuiTheme();
 
 		auto inputManager = new MyInputManager(window);
 
@@ -73,17 +61,17 @@ public:
 
 		while (!window.ShouldClose())
 		{
-			NuakeRenderer::Begin();
+			Begin();
 
 			// Reload html file.
 			if (glfwGetKey(window.GetHandle(), GLFW_KEY_0))
 			{
-				canvas = NuakeUI::CanvasParser::Get().Parse("../resources/UI/demo.html");
+				canvas = CanvasParser::Get().Parse("../resources/UI/demo.html");
 				canvas->SetInputManager(inputManager);
 			}
 
 			if (glfwGetKey(window.GetHandle(), GLFW_KEY_1))
-				NuakeUI::Renderer::Get().ReloadShaders();
+				Renderer::Get().ReloadShaders();
 
 			if (glfwGetKey(window.GetHandle(), GLFW_KEY_F5))
 			{
@@ -95,14 +83,12 @@ public:
 			canvas->Tick();
 			canvas->Draw();
 
-			NuakeUI::DrawInspector(canvas);
+			//NuakeUI::DrawInspector(canvas);
 
 			window.SwapBuffers();
 		}
 	}
-
 };
-
 
 void main()
 {
